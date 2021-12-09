832cbb7ac3e3a7e6433ac01fe021f78c118a32b0
Jonathon Marolf <jmarolf@microsoft.com>
2021-12-09 15:26:19 -0800

    simple utility that lists all tests in an arcade-style repo given a binlog file

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.Build.Logging.StructuredLogger;
+
+if (args.Length != 1)
+{
+    return;
+}
+
+var logFilePath = args[0];
+if (!File.Exists(logFilePath))
+{
+    return;
+}
+
+var build = Serialization.Read(logFilePath);
+
+var testProjectPaths = build
+    .FindChildrenRecursive<ProjectEvaluation>()
+    .Where(
+        project => project.FindChildrenRecursive<Property>(
+            property => property.Name == "IsTestProject" &&
+                        bool.TryParse(property.Value, out var value) && value).Any())
+    .SelectMany(project =>
+    {
+        var outputPath = project.FindChildrenRecursive<Property>(property => property.Name == "OutputPath").Single().Value;
+        var assemblyName = project.FindChildrenRecursive<Property>(property => property.Name == "AssemblyName").Single().Value;
+        var targetFrameworks = project.TargetFramework.Split(';');
+        return targetFrameworks.Length > 1
+            ? targetFrameworks.Select(tfm => Path.Combine(outputPath, tfm, assemblyName + ".dll"))
+            : (new[] { Path.Combine(outputPath, assemblyName + ".dll") });
+    })
+    .ToArray();
+
+Console.WriteLine($"Found {testProjectPaths.Length} test projects");
+Console.WriteLine();
+Console.WriteLine($"{testProjectPaths.Where(p => p.Contains("net472")).Count()} .NET Framework Tests");
+foreach (var testProjectPath in testProjectPaths.Where(p => p.Contains("net472")))
+{
+    Console.WriteLine("\t" + testProjectPath);
+}
+Console.WriteLine();
+Console.WriteLine($"{testProjectPaths.Where(p => !p.Contains("net472")).Count()} .NET Core Tests");
+foreach (var testProjectPath in testProjectPaths.Where(p => !p.Contains("net472")))
+{
+    Console.WriteLine("\t" + testProjectPath);
+}
